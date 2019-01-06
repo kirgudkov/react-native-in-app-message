487d6a50e28d2a6827e41c62f18935f84ba433a6
Kirill Gudkov <for.example.nickname@gmail.com>
2019-01-06 22:14:25 +0300

    add license

--- src/Noification/Props.ts
+++ src/Noification/Props.ts
@@ -3,8 +3,8 @@ import {ReactNode} from "react";
 export interface Props {
   blurType?: 'xlight' | 'light' | 'dark', // iOS Only
   blurAmount?: number, // iOS Only
-  duration: number,
-  autohide: boolean,
+  duration?: number,
+  autohide?: boolean,
   text?: string,
   textColor?: string,
   customComponent?: ReactNode,
